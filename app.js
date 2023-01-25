38418fe6172e351286d68676a77794716f319f94
Aakanksha <Aakanksha2013073@akgec.ac.in>
2023-01-25 23:25:06 +0530

    new change

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 function imagefun1() {
     var Image_Id = document.getElementById('getImage');
     var letter = document.getElementsByClassName('btn1').value;
-    if (Image_Id.src.match("img1.png")) {
-        Image_Id.src = "img2.png";
+    if (Image_Id.src.match("/img/img1.png")) {
+        Image_Id.src = "/img/img2.png";
     }
     else {
-        Image_Id.src = "img1.png";
+        Image_Id.src = "/img/img1.png";
     }
 }  
 function img() {
